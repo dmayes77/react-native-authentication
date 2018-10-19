c21350f4aa22e8c189479dde65b156b3e88fe76d
David Mayes <dmayes77@gmail.com>
2018-10-19 01:46:36 -0400

    setup login form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 export default class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAir1LNmmcmeRJHu7JfQ8kJHp8cCObGqU0',
+      authDomain: 'auth-e4039.firebaseapp.com',
+      databaseURL: 'https://auth-e4039.firebaseio.com',
+      projectId: 'auth-e4039',
+      storageBucket: 'auth-e4039.appspot.com',
+      messagingSenderId: '219128051918'
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <Text style={styles.welcome}>Hello, world!</Text>
+        <LoginForm />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  }
-});
