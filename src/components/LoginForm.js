2a0c4931a745f1e472ed33bdac3440eb41c64743
David Mayes <dmayes77@gmail.com>
2018-10-19 14:02:20 -0400

    added InputField to LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,30 +1,68 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { StyleSheet, View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Button, Card, CardSection, InputField } from './common';
 
 export default class LoginForm extends Component {
   state = {
-    text: ''
+    email: '',
+    password: '',
+    error: ''
+  };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+    this.setState({ error: '' });
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({ error: 'Authentication Failed.' });
+          });
+      });
   }
 
   render() {
     return (
+      <View>
         <Card>
           <CardSection>
-            <TextInput
-              value={this.state.text}
-              onChange={text => this.setState({ text })} 
-              style={{ height: 30, width: 100 }} 
+            <InputField
+              autoCapitalize='none'
+              label='Email'
+              onChangeText={email => this.setState({ email })}
+              placeholder='user@gmail.com'
+              value={this.state.email}
             />
           </CardSection>
-          <CardSection />
           <CardSection>
-            <Button>
+            <InputField
+              autoCapitalize='none'
+              label='Password'
+              onChangeText={password => this.setState({ password })}
+              placeholder='password'
+              secureTextEntry
+              value={this.state.password}
+            />
+          </CardSection>
+
+            <Text style={styles.errorStyle}>{this.state.error}</Text>
+
+          <CardSection>
+            <Button onPress={this.onButtonPress.bind(this)}>
               Log In
             </Button>
           </CardSection>
         </Card>
+      </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  errorStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+});
